db85c0897eab94d7828c2bd2a3392d889b3cce13
thesmartwon <thesmartwon@pm.me>
2024-01-01 18:38:30 -0500

    reload midi inputs on click

--- src/input/midi.tsx
+++ src/input/midi.tsx
@@ -10,22 +10,27 @@ export interface MIDISelectProps {
 export function MIDISelect({ value, setValue }: MIDISelectProps) {
 	const [inputs, setInputs] = useState<Input[]>([]);
 
-	useEffect(() => {
+	function loadInputs(setInitial = false) {
 		navigator.requestMIDIAccess().then(m => {
 			const inputs: Input[] = [undefined];
 			for (const entry of m.inputs.values()) inputs.push(entry);
 			setInputs(inputs);
-			setValue(inputs[inputs.length - 1]);
+			if (setInitial) setValue(inputs[inputs.length - 1]);
 		});
-	}, []);
+	}
+	useEffect(() => loadInputs(true), []);
 
 	return (
 		<>
 			MIDI Device
-			<select value={value?.id} onChange={ev => {
-				const input = inputs.find(i => i?.id == ev.currentTarget.value);
-				setValue(input);
-			}}>
+			<select
+				value={value?.id}
+				onChange={ev => {
+					const input = inputs.find(i => i?.id == ev.currentTarget.value);
+					setValue(input);
+				}}
+				onClick={() => loadInputs(false)}
+			>
 				{inputs.map(input =>
 					<option value={input?.id}>{input ? input.name : 'None'}</option>
 				)}
